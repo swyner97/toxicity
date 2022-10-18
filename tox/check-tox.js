847ec12c245339f3a923aef8a0dd7ff3e2274e7f
swyner97 <swyner97@gmail.com>
2022-10-17 21:31:57 -0700

    figured out truth assignments

--- tox/check-tox.js
+++ tox/check-tox.js
@@ -1,23 +1,21 @@
 const threshold = 0.5;
 const toxicity = require('@tensorflow-models/toxicity');
-
 toxicity.load(threshold).then((model) => {
     const sentences = ["You are a tiny, little baby poop", "My favorite color is blue.", "Shut up!"];
-
     model.classify(sentences).then((predictions) => {
         const toxic = predictions.filter(prediction => prediction.label === "toxicity");
-
+        // console.log(JSON.stringify(toxic))
+    
         const results = toxic[0].results;
+        console.log(results)
 
         for (let i = 0; i < results.length; i++) {
-
             let truthy = results[i].match;
-            
-            if (truthy === true){
-                console.log(truthy)
+            let sentence = sentences[i];
+            if (truthy) {
+                truthy === sentence; 
+                console.log(sentence)
             }
         }
     })
-
 });
-
