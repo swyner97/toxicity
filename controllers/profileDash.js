3c518175b81599791532ef95635e06b9332abb1f
shaq <school@robertos-MBP.home>
2022-10-24 21:23:17 -0700

    New Working File

--- controllers/profileDash.js
+++ controllers/profileDash.js
@@ -46,23 +46,23 @@ router.get('/profile', async (req, res) => {
 });
 
 
-router.get('/edit/:id', withAuth, async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id);
+// router.get('/edit/:id', withAuth, async (req, res) => {
+//   try {
+//     const postData = await Post.findByPk(req.params.id);
 
-    if (postData) {
-      const post = postData.get({ plain: true });
+//     if (postData) {
+//       const post = postData.get({ plain: true });
 
-      res.render('edit-post', {
-        layout: 'profileDash',
-        post,
-      });
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.redirect('login');
-  }
-});
+//       res.render('edit-post', {
+//         layout: 'profileDash',
+//         post,
+//       });
+//     } else {
+//       res.status(404).end();
+//     }
+//   } catch (err) {
+//     res.redirect('login');
+//   }
+// });
 
 module.exports = router;
